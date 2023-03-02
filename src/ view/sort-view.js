f93ca242ee50056b82748c0020a33c96b9d67484
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-03-02 15:33:29 +0100

    Поправит шаблон сортировки

--- src/ view/sort-view.js
+++ src/ view/sort-view.js
@@ -1,10 +1,12 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { SortType } from '../const.js';
+
 
 const createSortTemplate = () =>
   `<ul class="sort">
-  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-  <li><a href="#" class="sort__button">Sort by date</a></li>
-  <li><a href="#" class="sort__button">Sort by rating</a></li>
+  <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
 </ul>`;
 
 export default class SortView extends AbstractView {
---
+++ src/const.js
@@ -0,0 +1,14 @@
+const FilterType = {
+  WATCHLIST: 'Watchlist',
+  HISTORY: 'history',
+  FAVORITES: 'favorites',
+};
+
+
+const SortType = {
+  DEFAULT: 'default',
+  DATE: 'date',
+  RATING: 'rating',
+};
+
+export { FilterType, SortType };
